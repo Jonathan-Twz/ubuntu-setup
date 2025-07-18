a4e9bf3acc7633564b2f4c542634e2cfba33e59a
Wenzhe Tong <wenzhe.t99@gmail.com>
2025-07-17 23:37:33 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade -y
+
+# apt dev tool
+sudo apt install ca-certificates curl git vim htop 
+
+# snap dev tool
+sudo snap install code 
+
+# apt additional tool
+sudo apt install vlc simplescreenrecorder terminator gnome-tweaks
+
+# snap additional tool
+sudo snap install slack notion-snap-reborn zotero-snap discord
